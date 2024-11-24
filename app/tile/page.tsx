e8669aed232d892d14fbbd5610ded3493539a234
Pierre Wellner <pierre@pierrewellner.com>
2024-11-24 08:43:39 -0500

    Change Expert to Quality on tile page

--- app/tile/page.tsx
+++ app/tile/page.tsx
@@ -23,7 +23,7 @@ export default function Tile() {
         <div className="absolute inset-0 grid grid-rows-[60px_1fr_60px] items-end justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
           <div className="flex flex-col gap-3 row-start-2 items-center sm:items-start text-white bg-gray-900/50 p-10 px-30">
             <div className="font-bold">
-              Expert tile installation
+              Quality tile installation
             </div>
             <div>
               <ul className="list-inside list-disc text-sm pl-0 sm:text-left">
